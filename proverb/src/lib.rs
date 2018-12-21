137f7114b67d5fbe128a7f56022c07917c3911dd
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-21 23:20:45 +0200

    Proverb - better solution

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,25 +1,14 @@
-#![allow(unused)]
-
 pub fn build_proverb(list: &[&str]) -> String {
     match list.len() {
         0 => return "".to_string(),
-        1 => return format!("And all for the want of a {0}.", list.first().unwrap()),
         _ => {
-            let mut proverbs: Vec<String> = Vec::with_capacity(list.len());
-            let mut prev = list[0];
+            let last_piece = format!("And all for the want of a {0}.", list[0]);
 
-            for piece in &list[1..] {
-                let proverb: String = format!("For want of a {0} the {1} was lost.", prev, piece);
-                proverbs.push(proverb);
-                prev = piece;
-            }
-
-            proverbs.push(format!(
-                "And all for the want of a {0}.",
-                list.first().unwrap()
-            ));
-
-            return proverbs.join("\n");
+            list.windows(2)
+                .map(|w| format!("For want of a {0} the {1} was lost.", w[0], w[1]))
+                .chain(vec![last_piece])
+                .collect::<Vec<_>>()
+                .join("\n")
         }
     }
 }
