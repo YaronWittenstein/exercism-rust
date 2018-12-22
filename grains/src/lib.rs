fdf7698904538c795c2c87f6b0598ffc488fcf17
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-22 15:39:55 +0200

    grains - more succint solution

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    if s >= 1 && s <= 64 {
-        1 << (s - 1)
-    } else {
-        panic!("Square must be between 1 and 64");
+    match s {
+        1...64 => 1 << (s - 1),
+        _ => panic!("Square must be between 1 and 64"),
     }
 }
 
 pub fn total() -> u64 {
-    (1..65).map(|s| square(s)).sum()
+    (1..65).map(square).sum()
 }
