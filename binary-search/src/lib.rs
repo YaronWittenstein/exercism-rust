a54e9620b70a50095c11848e444937a2319c419a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-24 12:38:47 +0200

    Binary Search

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,34 +1,28 @@
+#![allow(unused)]
+
 pub fn find(numbers: &[i32], number: i32) -> Option<i32> {
-    if numbers.len() <= 0 {
+    if numbers.len() == 0 {
         return None;
     }
 
-    let mut start = 0;
-    let mut end = (numbers.len() - 1) as i32;
-    let mut res = 0;
+    let mut start: isize = 0;
+    let mut end: isize = (numbers.len() - 1) as isize;
 
-    let mut found = false;
-
-    while start <= end && found == false {
+    while start <= end {
         let mid = (start + end) / 2;
 
-        if mid >= numbers.len() as i32 {
-            return None;
+        let v: i32 = numbers[mid as usize];
+
+        if v == number {
+            return Some(mid as i32);
         }
 
-        let mid_num: i32 = numbers[mid as usize];
-        if mid_num == number {
-            res = mid;
-            found = true;
-        } else if mid_num > number {
+        if number < v {
             end = mid - 1;
         } else {
             start = mid + 1;
         }
     }
 
-    match found {
-        false => None,
-        true => Some(res as i32),
-    }
+    None
 }
