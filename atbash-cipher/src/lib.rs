b2bf34c470416317ab50e14e3b8b7fe061d9b4b3
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-31 14:26:49 +0200

    Atbash Cipher

---
+++ atbash-cipher/src/lib.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+pub fn encode(plain: &str) -> String {
+    transform(plain)
+        .chars()
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+pub fn decode(cipher: &str) -> String {
+    transform(cipher)
+}
+
+pub fn transform(text: &str) -> String {
+    let chr_map = build_table();
+
+    text.to_lowercase()
+        .chars()
+        .filter(|c| (c.is_ascii() && c.is_alphabetic()) || c.is_digit(10))
+        .map(|c| {
+            if let Some(&matched) = chr_map.get(&c) {
+                matched
+            } else {
+                c
+            }
+        })
+        .collect::<String>()
+}
+
+fn build_table() -> HashMap<char, char> {
+    let mut chr_map = HashMap::new();
+
+    (0..25).for_each(|i| {
+        let chr = char::from(b'a' + i);
+        let rev = char::from(b'z' - i);
+
+        chr_map.insert(chr, rev);
+        chr_map.insert(rev, chr);
+    });
+
+    chr_map
+}
