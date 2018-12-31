5892f9c6096069ff757a13bc55b4722650f59aac
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-31 23:14:56 +0200

    Crypto Square

---
+++ crypto-square/src/lib.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+
+use std::iter;
+
+struct Rectangle {
+    r: usize,
+    c: usize,
+    pad: usize,
+}
+
+pub fn encrypt(input: &str) -> String {
+    let chars = input
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphanumeric() || c.is_digit(10))
+        .collect::<Vec<_>>();
+
+    if chars.len() == 0 {
+        return String::from("");
+    }
+
+    let dims = calc_rectangle(chars.len());
+    let pads = iter::repeat(' ').take(dims.pad);
+
+    let chars_vec = chars.into_iter().chain(pads).collect::<Vec<_>>();
+    let mut strings = Vec::<String>::with_capacity(dims.c);
+
+    (0..dims.c).for_each(|c| {
+        let mut entry: Vec<char> = Vec::with_capacity(dims.r);
+
+        (0..dims.r).for_each(|r| {
+            let ch = chars_vec[r * dims.c + c];
+            entry.push(ch);
+        });
+
+        strings.push(entry.iter().collect());
+    });
+
+    strings.join(" ")
+}
+
+fn calc_rectangle(n: usize) -> Rectangle {
+    let (c, r) = (n..).find_map(|c| find_pair(c)).unwrap();
+
+    Rectangle {
+        c,
+        r,
+        pad: c * r - n,
+    }
+}
+
+fn find_pair(n: usize) -> Option<(usize, usize)> {
+    (1..n).find_map(|c| {
+        if c * c == n {
+            Some((c, c))
+        } else if c * (c - 1) == n {
+            Some((c, c - 1))
+        } else {
+            None
+        }
+    })
+}
