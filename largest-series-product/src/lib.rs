cc46e099c9a7ece66f24bbb52e7fb701260347f4
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2018-12-31 12:31:16 +0200

    Largest Series Product - shorter solution

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -14,12 +14,10 @@ pub fn lsp(string_digits: &str, n: usize) -> Result<u64, Error> {
         .map(|c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
         .try_fold(Vec::new(), |mut acc, digit_or_err| -> _ {
             if digit_or_err.is_ok() {
-                let digit = digit_or_err.unwrap();
-                acc.insert(0, digit as u64);
+                acc.insert(0, digit_or_err.unwrap() as u64);
                 Ok(acc)
             } else {
-                let err = digit_or_err.unwrap_err();
-                Err(err)
+                Err(digit_or_err.unwrap_err())
             }
         })?
         .windows(n)
