550fd02db23e958d58da2b75a5e4a92c59ecb725
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-01-01 13:25:11 +0200

    Sublist

---
+++ sublist/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let sublist1 = is_sublist(first_list, second_list);
+    let sublist2 = is_sublist(second_list, first_list);
+
+    match (sublist1, sublist2) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
+    }
+}
+
+fn is_sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
+    let wsize = first_list.len();
+
+    match wsize {
+        0 => true,
+        _ => second_list
+            .windows(wsize)
+            .any(|w| slices_equal(w, first_list)),
+    }
+}
+
+fn slices_equal<T: PartialEq>(s: &[T], t: &[T]) -> bool {
+    s.iter().enumerate().all(|(i, si)| *si == t[i])
+}
