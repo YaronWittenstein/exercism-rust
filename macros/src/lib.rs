845e8a7bd07fb301b7da42fb20e62b6b1537f764
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-01-03 12:10:54 +0200

    Macros - shorter solution

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,16 +1,9 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        std::collections::HashMap::new()
+    ( $( $key:expr => $val:expr ),*, ) => {
+        hashmap!($($key => $val),*)
     };
     ( $( $key:expr => $val:expr ),* ) => {
-        {
-            let mut h = std::collections::HashMap::new();
-            $( h.insert($key, $val); )*
-            h
-        }
-    };
-    ( $( $key:expr => $val:expr ),*, ) => {
         {
             let mut h = std::collections::HashMap::new();
             $( h.insert($key, $val); )*
